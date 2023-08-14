5ecc81bea19b4875991e21e0b46a7d897e8ea543
Akshit <116799118+akshitDhakad@users.noreply.github.com>
2023-08-14 14:46:14 +0530

    CRUD methods completed

--- index.js
+++ index.js
@@ -1,68 +1,136 @@
-const  fs = require("fs")
-const products = require("./data.json")
-// console.log(products)
 const express = require("express")
 const app = express()
-const { Server } = require("http")
-
-// const index = fs.readFileSync("index.html", "utf8")
-app.use(express.static("public"))
-
-app.use(express.json())
-// this middleware we use when we need to fetch data from forms
-// app.use(express.urlencoded())
-
-// server level middleware
-// app.use((req,res,next)=>{
-//   console.log(req.method,req.ip,req.hostname)
-//   next()
-// })
+const products = require("./data.json")
 
 
-// Route level middleware
+// In back end  we generally use 4 method  called  CRUD
+// Create POST / Product
+//******************** Create Data base **********************
+app.post("/products",(req,res)=>{
+  products.push(req.body)
+  res.status(201).json(req.body)
+})
 
-const auth = (req,res,next)=>{
-  if(req.body.password == "123"){
-    console.log(req.query)
-    next()
-  }else{
-    res.sendStatus(401)
+//******************** Read Data base **********************
+app.get("/products",(req,res)=>{
+  res.json(products)
+})
+app.get("/products/:id", (req, res) => {
+  const id = +req.params.id;
+  const product = products.products.find(p => p.id === id); // Use products.products to access the array
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ message: "Product not found" });
   }
-  
-}
+});
 
-//  API - Endpoint -  Route   
+// ********************** Update Data base *********************
+app.put("/products/:id", (req, res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex(p => p.id === id);
+  if (productIndex !== -1) {
+    products[productIndex] = { ...req.body, id: id };
+    res.status(201).json();
+  } else {
+    res.status(404).json({ message: "Product not found" });
+  }
+});
 
-app.get("/",auth,(req,res)=>{
-  res.json({Type:"GET"})
-})
-app.post("/",(req,res)=>{
-  res.json({Type:"post"})
-})
-app.put("/",(req,res)=>{
-  res.json({Type:"put"})
-})
-app.patch("/",(req,res)=>{
-  res.json({Type:"patch"})
-})
-app.delete("/",(req,res)=>{
-  res.json({Type:"delete"})
-})
 
-// app.get("/",(req, res) => {
-//   res.sendFile(__dirname + "/index.html")
-// })
+app.patch("/products/:id", (req, res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex(p => p.id === id);
+  const product = product[productIndex]
+  products.splice(productIndex,1,{product,...req.body})
+  res.status(201).json()
+  if (productIndex !== -1) {
+    products[productIndex] = { ...req.body, id: id };
+    res.status(201).json();
+  } else {
+    res.status(404).json({ message: "Product not found" });
+  }
+});
 
-// app.get("/products",(req, res) => {
-//   res.json(products)
-// })
+// ********************** Delete Data base *********************
+app.delete("/products/:id", (req, res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex(p => p.id === id);
+   const product = product[productIndex]
+   products.splice(productIndex,1)
+   res.status(201).json(product)
+});
 
 app.listen(3000,()=>{
   console.log("server running on port 3000")
 })
 
+// second old code 
+// const  fs = require("fs")
+// const products = require("./data.json")
+// // console.log(products)
+// const express = require("express")
+// const app = express()
+// const { Server } = require("http")
 
-// old code from here 
+// // const index = fs.readFileSync("index.html", "utf8")
+// app.use(express.static("public"))
+
+// app.use(express.json())
+// // this middleware we use when we need to fetch data from forms
+// // app.use(express.urlencoded())
+
+// // server level middleware
+// // app.use((req,res,next)=>{
+// //   console.log(req.method,req.ip,req.hostname)
+// //   next()
+// // })
+
+
+// // Route level middleware
+
+// // const auth = (req,res,next)=>{
+// // //   if(req.body.password == "123"){
+// // //     console.log(req.query)
+// // //     next()
+// // //   }else{
+// // //     res.sendStatus(401)
+// // //   }
+// //   next()
+// // }
+
+// //  API - Endpoint -  Route   
+
+// app.get("/products/:id",auth,(req,res)=>{
+//   console.log(req.params.id)
+// })
+// app.post("/",(req,res)=>{
+//   res.json({Type:"post"})
+// })
+// app.put("/",(req,res)=>{
+//   res.json({Type:"put"})
+// })
+// app.patch("/",(req,res)=>{
+//   res.json({Type:"patch"})
+// })
+// app.delete("/",(req,res)=>{
+//   res.json({Type:"delete"})
+// })
+
+// // app.get("/",(req, res) => {
+// //   res.sendFile(__dirname + "/index.html")
+// // })
+
+// // app.get("/products",(req, res) => {
+// //   res.json(products)
+// // })
+
+// app.listen(3000,()=>{
+//   console.log("server running on port 3000")
+// })
+
+
+// first old code from here 
 // // 116799118+akshitDhakad@users.noreply.github.com
 
 // const fs = require("fs");
